0d7b80b7297435ca489c9d984f57550ddd2f845e
Notitguy Office <kelvin77@seznam.cz>
2024-02-22 17:05:42 +0100

    end of basic part

---
+++ basic-tut/test_app.py
@@ -0,0 +1,26 @@
+#variable
+myFirstNumber = 5
+mySecondNumber = 6
+
+#list
+listOfNumbers = [1, 2, 3, 4]
+listOfNames = ['john', 'mike', 'steve']
+
+#condition
+# if myFirstNumber >= 0:
+#   print('number is positive')
+# else:
+#   print('number is negative')
+
+#loop
+# for name in listOfNames:
+#   print(name)
+
+#function
+def say_hello():
+  print('Hello world')
+# say_hello()
+
+#module
+import test_module as tm
+print(tm.sum(2,3))
